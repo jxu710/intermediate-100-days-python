a582b89eb55710617155ef7e94d24d1516c3a1be
Jingru Xu <nicoxu1000@gmail.com>
2023-10-13 16:58:42 -0400

    Finished TODO #5

--- main.py
+++ main.py
@@ -59,5 +59,35 @@ def check_sufficient(order):
         # print(order["ingredients"][item])
         if resources[item] < order["ingredients"][item]:
             print(f"Sorry there is not enough {resources[item]}")
+            return False
+        else:
+            return True
+
 
 check_sufficient(MENU[choice])
+
+# TODO 5. Process coins.
+#  a. If there are sufficient resources to make the drink selected, then the program should prompt the user to insert coins.
+#  b. Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
+#  c. Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
+#  pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
+
+if check_sufficient(MENU[choice]):
+    print("Please insert coins.")
+    quarters = int(input("how many quarters?: "))
+    dimes = int(input("how many dimes?:  "))
+    nickles = int(input("how many nickles?: "))
+    pennies = int(input("how many pennies?: "))
+
+    quarters_value = quarters * 0.25
+    dimes_value = dimes * 0.10
+    nickles_value = nickles * 0.05
+    pennies_value = pennies * 0.01
+
+    total_value_inserted = quarters_value + dimes_value + nickles_value+pennies_value
+
+    if total_value_inserted < MENU[choice]["cost"]:
+        print("Sorry that's not enough money. Money refunded.")
+    elif total_value_inserted >= MENU[choice]["cost"]:
+        change = round(total_value_inserted - MENU[choice]["cost"], 2)
+        print(f"Here is ${change} in change.\nHere is your {choice} ☕️. Enjoy!")
