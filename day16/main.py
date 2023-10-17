c7388a0198361f0d967412fb09dd73a95e5fddbb
Jingru Xu <nicoxu1000@gmail.com>
2023-10-16 21:33:03 -0400

    Add python package PrettyTable and played with it

--- day16/main.py
+++ day16/main.py
@@ -1,10 +1,20 @@
-from turtle import Turtle, Screen
-timmy = Turtle()
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+#
+#
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.fd(100)
+# timmy.speed(1)
+# my_screen = Screen()
+# my_screen.exitonclick()
 
+from prettytable import PrettyTable
 
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.fd(100)
-timmy.speed(1)
-my_screen = Screen()
-my_screen.exitonclick()
+table = PrettyTable()
+table.add_column('Pokemon Name', ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+table.align = "l"
+
+print(table)
