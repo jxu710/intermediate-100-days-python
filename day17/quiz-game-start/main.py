9ec90917b6ba1e65a286122a3dfc40b6937957b9
Jingru Xu <nicoxu1000@gmail.com>
2023-10-20 23:01:07 -0400

    add QuizBrain class to input to user the questions in question_bank

--- day17/quiz-game-start/main.py
+++ day17/quiz-game-start/main.py
@@ -1,11 +1,12 @@
 from question_model import  Question
 from data import question_data
-
+from quiz_brain import QuizBrain
 question_bank = []
 
 for item in question_data:
     data = Question(item["text"], item["answer"])
     question_bank.append(data)
 
-print(question_bank[0].text)
+quiz = QuizBrain(question_bank)
+quiz.next_question()
 
