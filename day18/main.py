493ee9914756e46d0c3ab564adcb23e8ccf8b8f0
Jingru Xu <nicoxu1000@gmail.com>
2023-11-05 00:54:39 -0400

    Add day 18's challenge 1 & 2

---
+++ day18/main.py
@@ -0,0 +1,34 @@
+from turtle import Turtle,Screen
+
+# nic = Turtle()
+# nic.shape("turtle")
+# nic.color("salmon")
+
+# challenge 1 : draw square
+# for _ in range (4):
+#     nic.forward(100)
+#     nic.right(90)
+
+# challenge 2 : draw a dotted line
+arrow = Turtle()
+arrow.shape("arrow")
+
+for _ in range (12):
+    arrow.forward(10)
+    arrow.color("white")
+    arrow.forward(10)
+    arrow.color("black")
+
+# instuction solution :
+# for _ in range (12):
+#     arrow.forward(10)
+#     arrow.penup()
+#     arrow.forward(10)
+#     arrow.pendown()
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
