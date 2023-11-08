212646c456278be6bce6b7d70044ae97199bb6a8
Jingru Xu <nicoxu1000@gmail.com>
2023-11-07 23:44:38 -0500

    Modify code to give random color instead of coming from a list

--- day18/challenge4.py
+++ day18/challenge4.py
@@ -1,16 +1,24 @@
 # Challenge 4 : random walk
 import random
+import turtle
 
 from turtle import Turtle,Screen
 
+turtle.colormode(255)
 line = Turtle()
 line.pensize(10)
 
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    my_color = (r,g,b)
+    return my_color
+
 directions = [0,90,180,270]
 
 for i in range(50):
-    line.color(random.choice(colours))
+    line.color(random_color())
     line.setheading(random.choice(directions))
     line.fd(30)
     line.speed('fast')
